1df25de774850bba35e01e8d18f4e3b70fd3cf5d
Hassanamiri <hassanamiri369@gmail.com>
2022-06-29 13:59:01 +0330

    create now route (detail route)

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,12 @@
 import Home from "../pages/Home/Home";
 import About from "../pages/About/About";
 import TodoApp from "../pages/TodoApp/TodoApp";
+import TodoDetails from "../pages/TodoApp/TodoDetails";
 
 
 export const routes = [
     {path : "/" , element : <Home/>},
     {path : "/about" , element : <About/>},
-    {path : "/todoApp" , element : <TodoApp/>}
+    {path : "/todoApp" , element : <TodoApp/>},
+    {path : "/todoApp/:id" , element : <TodoDetails/>}
 ]
