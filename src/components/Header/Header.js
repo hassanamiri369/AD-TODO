53c451aa660265718268bad109e383d05993a08d
Hassanamiri <hassanamiri369@gmail.com>
2022-07-07 16:33:56 +0330

    added dark mode and swithc toggle button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,33 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { HiMenu } from 'react-icons/hi'
+import { themeContext } from '../..'
+
+import Switch from "react-switch"
 
 import "./Style.css"
-const Header = ({openMenu , menu}) => {
+const Header = ({ openMenu, menu }) => {
+
+  const { theme, setTheme , toggleTheme } = useContext(themeContext)
+
+  
+
   return (
     <>
-    {/* <p>header</p> */}
-    <div >
-    {!menu && <HiMenu className='openIcon' onClick={openMenu}/>}
-    </div>
+      {/* <p>header</p> */}
+      
+      <div style={{ display: "flex", justifyContent: "space-between" , width : "100%" }} >
+        <div> {!menu && <HiMenu className='openIcon' onClick={openMenu} />}</div>
+        <div style={{marginRight : "30px"}}>
+          <Switch
+            // uncheckedIcon={true}
+            // checkedIcon={true}
+            onChange={toggleTheme}
+            checked={theme === "light"}
+          />
+         
+        </div>
+      </div>
+      
     </>
   )
 }
